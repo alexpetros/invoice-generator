deae19228513d30234d6f78ab1de181611a5c9e6
Alexander Petros <apetros15@gmail.com>
2024-01-27 16:47:47 -0500

    Add some more fields

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
 DB_NAME=invoices.db
 
 .PHONY: all
-all:
+all: $(DB_NAME)
+	@[[ -f "$(DB_NAME)" ]] || { echo 'Missing db; run `make db`' && exit 1; }
 	php -S localhost:8000 -t public
 
-.PHONY: reset-db
-reset-db:
-	rm -f $(DB_NAME)
+$(DB_NAME):
 	sqlite3 $(DB_NAME) < schema.sql
+
+.PHONY: delete-db
+delete-db:
+	rm -f $(DB_NAME)
+
+.PHONY: reset-db
+reset-db: delete-db $(DB_NAME)
+
--- public/index.php
+++ public/index.php
@@ -1,13 +1,78 @@
 <!DOCTYPE html>
-<?php require_once '../lib/start.php'; ?>
+<?php require_once '../lib/start.php' ?>
 
 <title>Invoice Generator</title>
 <script src="/static/vendor/htmx-1.9.10.js"></script>
 
 <style>
+body {
+  max-width: 700px;
+}
+
+dl.profile-details {
+  display: grid;
+  grid-template-columns: fit-content(30%) 1fr;
+
+}
+
+form {
+  label {
+    margin: .5rem 0;
+    display: block;
+  }
+}
 </style>
 
 <h1>Invoice Generator</h1>
 
-<button>Increase Count</button>
-<button>Decrease Count</button>
+
+<?php
+$profiles = db_query($db, 'SELECT * FROM profiles');
+$clients = db_query($db, 'SELECT * FROM businesses');
+$profile = $profiles[0]
+?>
+
+<section>
+<h2>Profiles</h2>
+<select name=profiles>
+<?php
+  array_map(function ($profile) {
+    echo '<option>'.$profile['business_name'].'</option>';
+  }, $profiles);
+?>
+</select>
+
+<dl class=profile-details>
+  <dt>Name</dt><dd><?= $profile["business_name"]?></dd>
+  <dt>Email</dt><dd><?= $profile["email"]?></dd>
+  <dt>Phone</dt><dd><?= $profile["phone"]?></dd>
+  <dt>Address</dt>
+  <dd><?= $profile["address_1"]?><br> <?= $profile["address_2"]?>
+  </dd>
+</dl>
+</section>
+
+<section>
+<h2>Clients</h2>
+<select name=clients>
+<?php
+  array_map(function ($client) {
+    echo '<option>'.$client['business_name'].'</option>';
+  }, $clients);
+?>
+</select>
+
+
+<section>
+<h2>Generate Invoice</h2>
+<form action=/invoice method=POST>
+<label>
+  Invoice #:
+  <input type=text placeholder="Leave blank to auto-generate">
+</label>
+<label>Issue Date:<input type=date></label>
+<label>Due Date:<input type=date></label>
+
+<button>Generate</button>
+</form>
+</section>
