c5eec2ce974f1ff71863489536445df9d9c0ed67
Alexander Petros <apetros15@gmail.com>
2024-01-29 11:19:19 -0500

    Add dynamic work item input

--- public/index.php
+++ public/index.php
@@ -13,79 +13,102 @@ $clients = db_query($db, 'SELECT * FROM clients');
 $today = (new DateTime())
 ?>
 
-<section>
 <form action=/invoices.php method=POST>
-<h2>Billing Details</h2>
 
-<label>
-<div>Profile</div>
-<select name=profile_id>
-  <option value="" required disabled>--select profile--</option>
+<section>
+  <h2>Billing Details</h2>
+  <label>
+  <div>Profile</div>
+  <select name=profile_id>
+    <option value="" required disabled>--select profile--</option>
+    <?php
+      array_map(function ($profile) {
+        ['name' => $name, 'profile_id' => $profile_id] = $profile;
+        echo "<option value=\"$profile_id\">$name</option>";
+      }, $profiles);
+    ?>
+  </select>
+  </label>
+</section>
+
+<section>
+  <label>
+  <div>Client</div>
+  <select name=client_id>
+    <option value="" required disabled>--select client--</option>
   <?php
-    array_map(function ($profile) {
-      ['name' => $name, 'profile_id' => $profile_id] = $profile;
-      echo "<option value=\"$profile_id\">$name</option>";
-    }, $profiles);
-  ?>
-</select>
-</label>
-
-<label>
-<div>Client</div>
-<select name=client_id>
-  <option value="" required disabled>--select client--</option>
-<?php
   array_map(function ($client) {
     ['name' => $name, 'client_id' => $client_id] = $client;
     echo "<option value=\"$client_id\">$name</option>";
   }, $clients);
-?>
-</select>
-</label>
-
-<p>
-<a href=/profiles.php>Add Profiles</a> --
-<a href=/clients.php>Add Clients</a>
-</p>
-
-<h2>Invoice Details</h2>
-<span class=muted>If no due date is provided, it will be automatically set to 2 weeks from the issue date.</span>
-<label>
-  <div>Invoice #:</div>
-  <input type=text name=invoice_number placeholder="Leave blank to auto-generate">
-</label>
-<label>
-  <div>Issue Date:</div>
-  <input type=date name=issue_date value="<?= $today->format('Y-m-d') ?>">
-</label>
-<label>
-  <div>Due Date:</div>
-  <input type=date name=due_date>
-</label>
-<p>
-
-<h2>Work Items</h2>
-
-<fieldset>
-<h3>1.</h3>
-<label>
-  <div>Description</div>
-  <input type=text>
-</label>
-<label>
-  <div>Additional Details</div>
-  <textarea></textarea>
-</label>
-<button type=button>Delete</button>
-</fieldset>
-
-<button type=button>Add Item</button>
-
-<h2>Finish</h2>
-<button>Generate</button>
-</form>
+  ?>
+  </select>
+  </label>
+  <p>
+  <a href=/profiles.php>Add Profiles</a> --
+  <a href=/clients.php>Add Clients</a>
+  </p>
 </section>
 
-<script>
+<section>
+  <h2>Invoice Details</h2>
+  <span class=muted>If no due date is provided, it will be automatically set to 2 weeks from the issue date.</span>
+  <label>
+    <div>Invoice #:</div>
+    <input type=text name=invoice_number placeholder="Leave blank to auto-generate">
+  </label>
+  <label>
+    <div>Issue Date:</div>
+    <input type=date name=issue_date value="<?= $today->format('Y-m-d') ?>">
+  </label>
+  <label>
+    <div>Due Date:</div>
+    <input type=date name=due_date>
+  </label>
+  <p>
+</section>
 
+<section id=work-items>
+  <h2>Work Items</h2>
+  <button type=button onclick="addWorkItem()" id=add-button>Add Item</button>
+</section>
+
+<section>
+  <h2>Finish</h2>
+  <button>Generate</button>
+  </form>
+</section>
+
+<template id=work-item-template>
+  <fieldset>
+    <h3>1.</h3>
+    <label> <div>Title</div> <input class=title type=text> </label>
+    <label> <div>Description</div> <textarea class=description></textarea> </label>
+    <button type=button onclick="deleteWorkItem(this.parentElement)">Delete</button>
+  </fieldset>
+</template>
+
+<script>
+function renumberWorkItems() {
+  const fieldsets = document.querySelectorAll('#work-items fieldset')
+  fieldsets.forEach((fieldset, i) => {
+    const num = i + 1
+    fieldset.querySelector('h3').innerText = `${num}.`
+    fieldset.querySelector('.title').setAttribute('name', `title-${num}`)
+    fieldset.querySelector('.description').setAttribute('name', `description-${num}`)
+  })
+}
+
+function addWorkItem() {
+  const template = document.querySelector('#work-item-template')
+  const button = document.querySelector('#add-button')
+  const newField = template.content.cloneNode(true)
+  button.before(newField)
+  renumberWorkItems()
+}
+
+function deleteWorkItem(fieldset) {
+  fieldset.remove()
+  renumberWorkItems()
+}
 </script>
